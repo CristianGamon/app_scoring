cb61ee253f2bddce2fd82b2b34992dc4ffc5a1e1
CristianGamon <156850203+CristianGamon@users.noreply.github.com>
2025-10-10 10:35:25 +0200

    Update app_scoring.py

--- app_scoring.py
+++ app_scoring.py
@@ -96,7 +96,7 @@ if st.sidebar.button('CALCULAR RIESGO'):
                      "type": "gauge",
                      "min": 0, "max": 100,
                      "startAngle": 180, "endAngle": 0,
-                     "radius": "95%",
+                     "radius": "100%",
                      "axisLine": {
                          "lineStyle": {
                              "width": 15,
@@ -167,6 +167,7 @@ if st.sidebar.button('CALCULAR RIESGO'):
 
 else:
     st.write('DEFINE LOS PARÁMETROS DEL PRÉSTAMO Y HAZ CLICK EN CALCULAR RIESGO')
+
 
 
 
